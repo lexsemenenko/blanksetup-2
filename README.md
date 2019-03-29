059130834d45e54180ac30d3dc757a28d62465a9
Lex Semenenko <lex@lex.me>
2019-03-29 12:12:00 -0400

    Updates

--- README.md
+++ README.md
@@ -2,5 +2,8 @@ This setup originally is based on Victor Hugo
 
 ## Changelog
 
+- 2019-03-29
+  - Updated to new npm packages
+  - Added Babel and eslint
 - 2018-12-09
   - Changed Sass to Less
--- webpack.common.js
+++ webpack.common.js
@@ -1,65 +1,98 @@
-const webpack = require("webpack");
-const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const AssetsPlugin = require("assets-webpack-plugin");
+const webpack = require('webpack')
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const AssetsPlugin = require('assets-webpack-plugin')
+const lessLists = require('less-plugin-lists')
 
 module.exports = {
   entry: {
-    main: path.join(__dirname, "src", "index.js")
+    main: path.join(__dirname, 'src', 'index.js')
   },
 
   output: {
-    path: path.join(__dirname, "dist")
+    path: path.join(__dirname, 'dist')
   },
 
   module: {
     rules: [
       {
         test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader?name=/[hash].[ext]"
+        loader: 'file-loader?name=/[hash].[ext]'
       },
 
-      { test: /\.json$/, loader: "json-loader" },
+      { test: /\.json$/, loader: 'json-loader' },
 
       {
-        loader: "babel-loader",
+        loader: 'babel-loader',
         test: /\.js?$/,
         exclude: /node_modules/,
         query: { cacheDirectory: true }
       },
 
+      // Webpack will use this loaders for any css files that it finds in the
+      // index.js file, because this is our entry, or in another file which is imported in index.js
+
       {
-        test: /\.(le|c)ss$/,
+        test: /\.(le|c)ss$/, // For what files are the loaders
         exclude: /node_modules/,
-        use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "less-loader"
-        ]
+        use: [{ // What Loaders to use from bottom to top
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: MiniCssExtractPlugin.loader // Extracts CSS into separate files. It creates a CSS file per JS file which contains CSS
+        }, {
+          loader: 'css-loader' // Second, translates CSS into CommonJS
+        }, {
+          loader: 'postcss-loader'
+        }, {
+          loader: 'less-loader',
+          options: {
+            plugins: [
+              new lessLists()
+            ]
+          }
+        }]
       }
     ]
   },
 
   plugins: [
     new webpack.ProvidePlugin({
-      fetch:
-        "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+      fetch: 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
     }),
 
     new AssetsPlugin({
-      filename: "webpack.json",
-      path: path.join(process.cwd(), "site/data"),
+      filename: 'webpack.json',
+      path: path.join(process.cwd(), 'site/data'),
       prettyPrint: true
     }),
 
     new CopyWebpackPlugin([
       {
-        from: "./src/fonts/",
-        to: "fonts/",
+        from: './src/fonts/',
+        to: 'fonts/',
+        flatten: true
+      },
+      {
+        from: './src/img/',
+        to: 'img/',
         flatten: true
       }
-    ])
+    ]),
+
+    // Add jQuery
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
   ]
-};
+}
+
+// =============================================================================
+// Notes
+// =============================================================================
+
+// # Loaders
+//                  A utility to help webpack to compile non-js resource types
+//                  such as, css, assets, etc.
